b72c643bbe6e767d259f9b296298d2812c090e38
Benjamin Spille <benjaminspille@Benjamins-MBP.home>
2017-08-22 18:51:40 -0400

    added the first form element

--- src/forms/cat-form.js
+++ src/forms/cat-form.js
@@ -5,31 +5,9 @@ import { bindActionCreators } from "redux";
 import * as actionCreators from "../actions";
 import { Field, Form, reduxForm } from "redux-form";
 
-function RenderFields(fields){
-    fields = fields.map((field)=>{
-        console.log(field);
-        return(
-            <Field
-                component = { field.component }
-                label = { field.label }
-                name = { field.name }
-                type = { field.type }
-            />
-        )
-    });
-    return fields;
-}
 
-function renderField(field){
-    console.log(field);
-    return(
-        <div className="input-row">
-            <input {...field.input} type={field.type}/>
-            {field.meta.touched && field.meta.error &&
-            <span className="error">{field.meta.error}</span>}
-        </div>
-    )
-}
+
+
 
 class CatForm extends Component {
     constructor(props){
@@ -37,11 +15,26 @@ class CatForm extends Component {
         console.log(this);
     }
 
-
+    renderField ({ input, label, type, meta: { touched, error, warning } }){
+        return(
+            <div>
+                <label>{label}</label>
+                <div>
+                    <input {...input} placeholder={label} type={type}/>
+                    {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+                </div>
+            </div>)
+    }
     render(){
         return(
-            <Form onSubmit={ this.props.handleSubmit()}>
-                <RenderFields fields={ this.props.fields }/>
+            <Form onSubmit={ this.props.handleSubmit(this.props.addNewCat)}>
+                <Field
+                    component= { this.renderField }
+                    lable= "Name"
+                    name= "name"
+                    type="text"
+                />
+                
             </Form>
         )
     }
@@ -58,47 +51,6 @@ function validate(value){
     console.log(value);
 }
 export default reduxForm({
-    fields:[
-        {
-            component: renderField,
-            label: "Name",
-            name: "name",
-            type: "text",
-        },
-        {
-            component: renderField,
-            label: "Queen",
-            name: "queenId",
-            type: "text",
-        },
-        {
-            component: renderField,
-            label: "Tom",
-            name: "tomId",
-            type: "text",
-        },
-        {
-            component: renderField,
-            label: "Date of Birth",
-            name: "birthday",
-            type: "date",
-        },
-        {
-            component: renderField,
-            label: "Image Url",
-            name: "imageUrl",
-            type: "text",
-        },
-        {
-            component: renderField,
-            label: "Owner",
-            name: "owner",
-            type: "text",
-        },
-
-
-
-    ],
     form: "newCat",
     validate
 })(connect(mapStateToProps, mapDispatchToProps)(CatForm));
